bfd670fc1041c07bad82d31b1de8f32412a5a138
krushna <krushnch442@gmail.com>
2026-03-24 15:55:30 +0530

    vercel origin added

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ CORS setup
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000","https://rsb-global.vercel.app/"],
+    origin: ["http://localhost:5173", "http://localhost:3000","https://rsb-global.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
